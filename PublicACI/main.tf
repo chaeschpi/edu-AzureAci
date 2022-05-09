88e0909f315d8163dccc9176bcb6ef633f7aa580
Daniel Kasper <daniel.kasper@umb.ch>
2022-05-09 14:11:29 +0200

    initial

---
+++ PublicACI/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_resource_group" "rgAci" {
+  name     = "rg-aci-euw"
+  location = "West Europe"
+
+}
+
+resource "azurerm_container_group" "aci" {
+  resource_group_name = azurerm_resource_group.rgAci.name
+  location            = azurerm_resource_group.rgAci.location
+  name                = "aci-demowebsite-Public"
+  os_type             = "Linux"
+
+  
+  
+  container {
+    name   = "demo-web-site"
+    image  = "${data.azurerm_container_registry.acr.login_server}/demowebsite:latest"
+    cpu    = "1"
+    memory = "0.5"
+    
+
+
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+  }
+  image_registry_credential {
+    server   = data.azurerm_container_registry.acr.login_server
+    username = data.azurerm_container_registry.acr.admin_username
+    password = data.azurerm_container_registry.acr.admin_password
+  }
+  lifecycle {
+    ignore_changes =[
+      tags,
+    ]
+  }
+}
+
---
+++ PublicACI/variables.tf
@@ -0,0 +1,15 @@
+variable "acrName" {
+  type = string
+  default = "acrdemoeuw9986"
+}
+variable "acrId" {
+    type = string
+    default = "/subscriptions/beb82784-53ff-46c5-bd24-e96118a63038/resourceGroups/rg-acr-euw/providers/Microsoft.ContainerRegistry/registries/acrdemoeuw9986"
+  
+}
+
+variable "acrRgName" {
+  
+  type = string
+  default = "rg-acr-euw"
+}
